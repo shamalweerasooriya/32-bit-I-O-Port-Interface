00583a39521b94e13ec23ed16165db548108f6ab
shamalweerasooriya <e17380@eng.pdn.ac.lk>
2022-02-01 21:49:50 +0530

    generate hex files

--- src/readTest.c
+++ src/readTest.c
@@ -3,10 +3,7 @@
 int main(void)
 {
     // Reads a 32 bit value from the 32 bit port
-    int32_t value = Read_32();
-
-    // Prints the value
-    printf("%d\n", value);
+    uint32_t value = Read_32();
 
     return 0;
 }
